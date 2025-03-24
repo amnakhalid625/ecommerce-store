a42958271c0bbc0700925e2d889fe5e01082ce85
amnakhalid625 <amnakhalidgsr@gmail.com>
2025-03-24 12:05:06 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './App.css'
+import Header from './components/header/Header';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
 
 function App() {
 
   return (
-   <>
-   world
-   </>
+    <>
+      <BrowserRouter>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Header />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
